155b6fd0abc066f422508fd99fe695e60e395e36
Willian Arcênio <willianarcenio@hotmail.com>
2022-04-07 10:53:43 -0300

    Add Loading na listagem de Cursos + Alterando o nome da variável 'courses' do arquivo 'courses.component.ts' para 'courses$' pra identificar que é 'Observable'.

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -10,7 +10,7 @@ import { CoursesService } from '../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses: Observable<Course[]>;
+  courses$: Observable<Course[]>;
   //courses: Course[] = [];
   displayedColumns = ['name', 'category'];
 
@@ -22,7 +22,7 @@ export class CoursesComponent implements OnInit {
   {
     // this.courses = [];
     //this.coursesService = new CoursesService();
-    this.courses = this.coursesService.list();
+    this.courses$ = this.coursesService.list();
 
     // this.coursesService.list().subscribe(courses => this.courses = courses);
   }
--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 
 import { Course } from '../model/course';
 import { HttpClient } from '@angular/common/http';
-import { tap, first } from 'rxjs/operators';
+import { tap, first, delay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,6 +18,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
+      delay(2000),
       tap(courses => console.log(courses))
     );
   }
